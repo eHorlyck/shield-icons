ce11b8dd62ad5353431b1b9b26c3473075360b7a
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 04:41:46 +0600

    Create Copy Badge with 2 click

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import { CopyIcon } from '@primer/octicons-react'
+import { CopyIcon, CheckCircleFillIcon } from '@primer/octicons-react'
 
 class Badge extends Component {
 
     state = {
         isHover: false,
+        value: '',
+        copied: false,
     }
     toggleHover() {
         this.setState({ isHover: !this.state.isHover })
+    }
+    toggleClick(markdown) {
+        // console.log(markdown)
+        this.setState({
+            value: markdown
+        })
+
     }
 
     render() {
@@ -25,12 +34,25 @@ class Badge extends Component {
         // const markdown=`## Hi`
 
         return (
-            <div className="card-body text-center overflow-auto" style={{ minHeight: '80px' }}
-                onMouseEnter={() => this.toggleHover()}
-                onMouseLeave={() => this.toggleHover()}
-            >
-                {this.state.isHover ? <CopyIcon /> : <ReactMarkdown children={markdown} />}
-            </div>
+            <CopyToClipboard text={this.state.value}>
+                <div className="card-body text-center overflow-auto" style={{ minHeight: '80px' }}
+                    onMouseEnter={() => this.toggleHover()}
+                    onMouseLeave={() => this.toggleHover()}
+                    // onClick={() => this.toggleClick(markdown)}
+                    onClick={() => this.setState({ value: markdown, copied: true })}
+                >
+
+                    {this.state.isHover ?
+                        this.state.copied ? <CheckCircleFillIcon /> :
+                            <CopyIcon aria-label='Double Click To Copy' />
+                        :
+                        <ReactMarkdown children={markdown} />
+                    }
+
+                </div>
+
+            </CopyToClipboard>
+
         )
     }
 };
