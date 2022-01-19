03ce0d6dac3a412a4f3158372df4628b2888c3c2
mrhrifat <mrhrifat383759@gmail.com>
2022-01-19 23:34:30 +0600

    Create Web Brand Name & Text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+const simpleIcons = require('simple-icons');
 
 const Header = () => {
     return (
-        <div>
-
+        <div className='container py-5'>
+            <div className="row">
+                <div className="col-md-6">
+                    <h3>Web Badge</h3>
+                    <p>2152 Free Badges of popular brands</p>
+                </div>
+                <div className="col-md-6">
+                    <a href="" className="">
+                        
+                    </a>
+                </div>
+            </div>
         </div>
     )
 };
