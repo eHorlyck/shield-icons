b51edb34f3434ea2b23668cecabe9ce75e42153d
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 02:08:59 +0600

    Create Backgrounf Color Available on Footer

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -10,13 +10,13 @@ const IconCard = ({ data }) => {
                 <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
                     <div className='card'>
                         <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
-                            <SVGIcon svg={item.svg} alt={item.title}/>
+                            <SVGIcon svg={item.svg} alt={item.title} />
                         </div>
                         <div className="card-body text-center overflow-auto">
                             <Badge title={item.title} hex={item.hex} />
                         </div>
-                        <div className="card-footer">
-                            <small>{item.hex}</small>
+                        <div className="card-footer" style={{ backgroundColor: `#${item.hex}` }}>
+                            <small className='text-light'>{`#${item.hex}`}</small>
                         </div>
                     </div>
                 </div>
