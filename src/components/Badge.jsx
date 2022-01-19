8a40bf15372fe40eac6e971b10e734a7810aed5c
mrhrifat <mrhrifat383759@gmail.com>
2022-01-19 21:18:35 +0600

    Change RegExp to solve 404 badge not found issue

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const Badge = ({ title, hex }) => {
-    // console.log(title)
+    console.log(title[31])
     // console.log(hex)
     const matchTitle=new RegExp(/\s/g)
-    // console.log(title.match(matchTitle))
-    const relpaceTitle=title.replace(matchTitle,'-')
+    console.log(title.match(matchTitle))
+    const relpaceTitle=title.replace(matchTitle,'%20')
 
     const markdown = `![](https://img.shields.io/badge/${relpaceTitle}-${hex}?style=for-the-badge&logo=${relpaceTitle}&logoColor=white)`
     // console.log(markdown)
