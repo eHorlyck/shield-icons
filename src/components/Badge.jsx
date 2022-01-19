4ded79b8e26392e1c5a82fc079235da3545d1222
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 02:31:48 +0600

    Convert Badge component Functional to Class

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { CopyToClipboard } from 'react-copy-to-clipboard'
 
-const Badge = ({ title, hex }) => {
-    // console.log(title[31])
-    // console.log(hex)
-    const matchTitle=new RegExp(/\s/g)
-    // console.log(title.match(matchTitle))
-    const relpaceTitle=title.replace(matchTitle,'%20')
+class Badge extends Component {
 
-    const markdown = `![](https://img.shields.io/badge/${relpaceTitle}-${hex}?style=for-the-badge&logo=${relpaceTitle}&logoColor=white)`
-    // console.log(markdown)
-    // const markdown=`## Hi`
+    render() {
+        const { title, hex } = this.props
+        // console.log(title[31])
+        // console.log(hex)
+        const matchTitle = new RegExp(/\s/g)
+        // console.log(title.match(matchTitle))
+        const relpaceTitle = title.replace(matchTitle, '%20')
 
-    return (
-        <>
-            <ReactMarkdown children={markdown} />
-        </>
-    )
+        const markdown = `![](https://img.shields.io/badge/${relpaceTitle}-${hex}?style=for-the-badge&logo=${relpaceTitle}&logoColor=white)`
+        // console.log(markdown)
+        // const markdown=`## Hi`
+        
+        return (
+            <>
+                <ReactMarkdown children={markdown} />
+            </>
+        )
+    }
 };
 
 export default Badge;
