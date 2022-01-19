89fa1d779f0c903b1a2b01070a3cba4f12cddb91
mrhrifat <mrhrifat383759@gmail.com>
2022-01-19 22:17:41 +0600

    Comment out console line

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -5,7 +5,7 @@ const Badge = ({ title, hex }) => {
     console.log(title[31])
     // console.log(hex)
     const matchTitle=new RegExp(/\s/g)
-    console.log(title.match(matchTitle))
+    // console.log(title.match(matchTitle))
     const relpaceTitle=title.replace(matchTitle,'%20')
 
     const markdown = `![](https://img.shields.io/badge/${relpaceTitle}-${hex}?style=for-the-badge&logo=${relpaceTitle}&logoColor=white)`
