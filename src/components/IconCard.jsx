1a348a5afb84525bae392b7e3041b04b5216979c
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 03:07:23 +0600

    Create Icon Appear on Hover Badge

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
 import SVGIcon from './SVGIcon'
 import Badge from './Badge'
+import { CopyIcon } from '@primer/octicons-react'
 
 
 class IconCard extends Component {
+
+    state = {
+        isHover: false,
+    }
+
+
+    toggleHover() {
+        this.setState({ isHover: !this.state.isHover })
+    }
 
     render() {
         const { data } = this.props
@@ -15,8 +25,13 @@ class IconCard extends Component {
                             <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
                                 <SVGIcon svg={item.svg} alt={item.title} />
                             </div>
-                            <div className="card-body text-center overflow-auto">
-                                <Badge title={item.title} hex={item.hex} />
+
+                            <div
+                                className="card-body text-center overflow-auto"
+                                onMouseEnter={()=>this.toggleHover()}
+                                onMouseLeave={()=>this.toggleHover()}
+                            >
+                                {this.state.isHover ? <CopyIcon/> : <Badge title={item.title} hex={item.hex} />}
                             </div>
                             <div className="card-footer" style={{ backgroundColor: `#${item.hex}` }}>
                                 <small className='text-light'>{`#${item.hex}`}</small>
