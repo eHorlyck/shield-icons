fb5f1a378590a847851e07659b8a41ff026e38c2
mrhrifat <mrhrifat383759@gmail.com>
2022-01-19 20:19:31 +0600

    Create Badge with Markdown

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
 
-const Badge = () => {
+const Badge = ({ title, hex }) => {
+    // console.log(title)
+    // console.log(hex)
+    const markdown = `![](https://img.shields.io/badge/${title}-${hex}?style=for-the-badge&logo=${title}&logoColor=white)`
+    // console.log(markdown)
+    // const markdown=`## Hi`
+
     return (
-        <div>
-
-        </div>
+        <>
+            <ReactMarkdown children={markdown} />
+        </>
     )
 };
 
