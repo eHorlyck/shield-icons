f97396a5b47c193c6a310fd67d2c6d78f6f15a93
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 03:15:27 +0600

    Create OnHover Effect on Badge

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { CopyIcon } from '@primer/octicons-react'
 
 class Badge extends Component {
+
+    state = {
+        isHover: false,
+    }
+    toggleHover() {
+        this.setState({ isHover: !this.state.isHover })
+    }
 
     render() {
         const { title, hex } = this.props
@@ -15,11 +23,14 @@ class Badge extends Component {
         const markdown = `![](https://img.shields.io/badge/${relpaceTitle}-${hex}?style=for-the-badge&logo=${relpaceTitle}&logoColor=white)`
         // console.log(markdown)
         // const markdown=`## Hi`
-        
+
         return (
-            <>
-                <ReactMarkdown children={markdown} />
-            </>
+            <div className="card-body text-center overflow-auto" style={{ minHeight: '80px' }}
+                onMouseEnter={() => this.toggleHover()}
+                onMouseLeave={() => this.toggleHover()}
+            >
+                {this.state.isHover ? <CopyIcon /> : <ReactMarkdown children={markdown} />}
+            </div>
         )
     }
 };
