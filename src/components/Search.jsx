d28316ecba9ebed19d44f96483b76525da0c55c1
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 01:03:53 +0600

    Create Input Seacrh Bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-
+import { RepoForkedIcon, PersonIcon, LawIcon } from '@primer/octicons-react'
 class Search extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='container'>
+                <div className="row">
+                    <div className="col-md-3">
+                        <input type="text" className="form-control" placeholder="Search by brand..." />
+                    </div>
+                   
+                </div>
             </div>
         )
     }
