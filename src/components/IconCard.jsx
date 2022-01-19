f561eddc8b608cbc6f72f528ffe30cb341359ea9
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 02:20:33 +0600

    Change IconCard Functional to Class Based Components

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -1,27 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import SVGIcon from './SVGIcon'
 import Badge from './Badge'
 
 
-const IconCard = ({ data }) => {
-    return (
-        <div className="row">
-            {data.map((item) => (
-                <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
-                    <div className='card'>
-                        <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
-                            <SVGIcon svg={item.svg} alt={item.title} />
-                        </div>
-                        <div className="card-body text-center overflow-auto">
-                            <Badge title={item.title} hex={item.hex} />
-                        </div>
-                        <div className="card-footer" style={{ backgroundColor: `#${item.hex}` }}>
-                            <small className='text-light'>{`#${item.hex}`}</small>
+class IconCard extends Component {
+
+    render() {
+        const { data } = this.props
+        return (
+            <div className="row">
+                {data.map((item) => (
+                    <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
+                        <div className='card'>
+                            <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
+                                <SVGIcon svg={item.svg} alt={item.title} />
+                            </div>
+                            <div className="card-body text-center overflow-auto">
+                                <Badge title={item.title} hex={item.hex} />
+                            </div>
+                            <div className="card-footer" style={{ backgroundColor: `#${item.hex}` }}>
+                                <small className='text-light'>{`#${item.hex}`}</small>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
-        </div>
-    )
+                ))}
+            </div>
+        )
+    }
 }
 export default IconCard
