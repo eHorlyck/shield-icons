f03db8ccf5aff2f16d4334dbc92a49f664eeea54
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 05:31:14 +0600

    Add link of different octicons

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { RepoForkedIcon, PersonIcon, LawIcon, StarIcon, HeartIcon } from '@primer/octicons-react'
-import Dark from './Dark';
+
 
 
 
@@ -13,33 +13,30 @@ class Search extends Component {
                     <div className="col-md-3  align-self-center ">
                         <input type="text" className="form-control" placeholder="Search by brand..." />
                     </div>
-                    <div className="col-md-2">
-                        {/* <Dark /> */}
-                    </div>
-                    <div className="col-md-7" >
+                    <div className="col-md-9" >
                         <ul className="nav justify-content-end align-items-center">
                             <li className="nav-item" style={{ width: '63px' }} >
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://github.com/mrhrifat" target='_blank'>
                                     {<PersonIcon size='medium' fill='#000' aria-label="Add new item" />}
                                 </a>
                             </li>
                             <li className="nav-item" style={{ width: '70px' }} >
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank'>
                                     {<RepoForkedIcon size='medium' fill='#000' aria-label="Add new item" />}
                                 </a>
                             </li>
                             <li className="nav-item" style={{ width: '70px' }} >
-                                <a className="nav-link" href="#">
-                                    {<LawIcon size='medium' fill='#000' aria-label="Add new item" />}
-                                </a>
-                            </li>
-                            <li className="nav-item" style={{ width: '70px' }} >
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank'>
                                     {<StarIcon size='medium' fill='#000' aria-label="Add new item" />}
                                 </a>
                             </li>
                             <li className="nav-item" style={{ width: '70px' }} >
-                                <a className="nav-link" href="#">
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank'>
+                                    {<LawIcon size='medium' fill='#000' aria-label="Add new item" />}
+                                </a>
+                            </li>
+                            <li className="nav-item" style={{ width: '70px' }} >
+                                <a className="nav-link" href="#" target='_blank'>
                                     {<HeartIcon size='medium' fill='#000' aria-label="Add new item" />}
                                 </a>
                             </li>
