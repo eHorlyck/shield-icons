71684f5ef63eb9f87750648316af71c22ec769da
mrhrifat <mrhrifat383759@gmail.com>
2022-01-19 21:09:57 +0600

    Update JSON data

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -4,7 +4,11 @@ import ReactMarkdown from 'react-markdown';
 const Badge = ({ title, hex }) => {
     // console.log(title)
     // console.log(hex)
-    const markdown = `![](https://img.shields.io/badge/${title}-${hex}?style=for-the-badge&logo=${title}&logoColor=white)`
+    const matchTitle=new RegExp(/\s/g)
+    // console.log(title.match(matchTitle))
+    const relpaceTitle=title.replace(matchTitle,'-')
+
+    const markdown = `![](https://img.shields.io/badge/${relpaceTitle}-${hex}?style=for-the-badge&logo=${relpaceTitle}&logoColor=white)`
     // console.log(markdown)
     // const markdown=`## Hi`
 
