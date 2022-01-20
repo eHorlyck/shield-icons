4d093c763883c5a026d6aeecec7464e894b191bd
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 19:25:46 +0600

    Update state & view layout

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,49 +1,29 @@
 import React, { Component } from 'react'
-import icons from './services/icons'
 import IconCard from './components/IconCard'
 import Header from './components/Header'
-import Search from './components/Search'
+import simpleClass from './services/main'
 
 
-const icon = new icons()
+const data = new simpleClass()
 
 class Root extends Component {
 
     state = {
-        data: icon.getIcon(),
+        data: data.getIcon()
     }
-
-    getBadge = (e) => {
-        console.log(e)
-        // console.log(this.state.data.map(item => console.log(item.name)))
-        // const searchData = this.state.data.filter(item => item.title.toLowerCase().includes(search.toLowerCase()))
-        const searched = this.state.data.filter(
-            item => {
-                if (e === '') {
-                    return item
-                } else if (item.title.toLowerCase().includes(e.toLowerCase())) {
-                    console.log(item)
-                    return item
-                }
-            }
-        )
-        this.setState({ data:searched })
-    }
-    
 
     render() {
-        console.log(this.state.data)
+        // console.log(this.state.data)
         const data = this.state.data
 
         return (
             <>
                 <>
                     <Header />
-                    <Search getBadge={this.getBadge} />
                 </>
-                <div className='container'>
+                <>
                     <IconCard data={data} />
-                </div>
+                </>
             </>
         )
     }
---
+++ src/services/main.js
@@ -0,0 +1,25 @@
+const simpleIcons = require('simple-icons');
+const slug = require('../data/slug.json')
+
+// console.log(simpleIcons.Get('hackerearth'))
+
+
+
+export default class simpleClass {
+
+    getIcon() {
+        // console.log(slug.brand)
+        return slug.brand.map(item => {
+            // console.log(item)
+            // console.log(`${item.slug}`)
+            const obj = simpleIcons.Get(`${item.slug}`)
+            return {
+                slug: obj.slug,
+                title: obj.title,
+                hex: obj.hex,
+                svg: obj.svg,
+            }
+        })
+    }
+
+}
