af8d835b88d006e82f6eae323d93cc61bd8984f2
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 19:00:27 +0600

    Add Search Component & use hooks to Search Data

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -1,39 +1,56 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import SVGIcon from './SVGIcon'
 import Badge from './Badge'
+import Search from './Search'
 
 
-class IconCard extends Component {
+const IconCard = ({ data }) => {
 
-    render() {
-        const { data } = this.props
-        return (
-            <div className="row">
-                {data.map((item) => (
-                    <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
-                        <div className='card'>
-                            <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
-                                <SVGIcon svg={item.svg} alt={item.title} />
-                            </div>
-                            < >
-                                <Badge title={item.title} hex={item.hex} />
-                            </>
+    const [search, setSearch] = useState('')
 
-                            <div
-                                className="card-footer text-center"
-                                style={{ backgroundColor: `#555555`, cursor: 'pointer' }}
-                            >
-                                 <small className='text-light'>
+    return (
+        <>
+            <Search search={(e) => setSearch(e)} />
+
+
+
+            <div className="container">
+
+                
+
+                <div className="row">
+                    {data.filter(item => {
+                        if (search === '') {
+                            return item
+                        } else if (item.title.toLowerCase().includes(search.toLowerCase())) {
+                            return item
+                        }
+                    }).map((item) => (
+                        <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
+                            <div className='card'>
+                                <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
+                                    <SVGIcon svg={item.svg} alt={item.title} />
+                                </div>
+                                < >
+                                    <Badge title={item.title} hex={item.hex} />
+                                </>
+
+                                <div
+                                    className="card-footer text-center"
+                                    style={{ backgroundColor: `#555555`, cursor: 'pointer' }}
+                                >
+                                    <small className='text-light'>
                                         {
                                             `${item.title}`
                                         }
                                     </small>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
-        )
-    }
+        </>
+    )
 }
 export default IconCard
