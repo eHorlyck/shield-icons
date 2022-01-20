680fba4a0f624e688f442e1f0fa0407dd2c55e53
mrhrifat <mrhrifat383759@gmail.com>
2022-01-21 02:21:16 +0600

    Create Small Bootstrapp Spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div class="text-center">
+            <div class="spinner-border spinner-border-sm text-secondary" role="status">
+            </div>
+        </div>
+    )
+};
+
+export default Spinner;
