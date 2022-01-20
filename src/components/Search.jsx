664dde5cfe03c6b81980fe2e635d2042fadae000
mrhrifat <mrhrifat383759@gmail.com>
2022-01-20 11:42:16 +0600

    Revert Search to Class Component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,46 +4,48 @@ import { RepoForkedIcon, PersonIcon, LawIcon, StarIcon, HeartIcon } from '@prime
 
 
 
-const Search = () => {
+class Search extends Component {
 
-    return (
-        <div className='container pb-3'>
-            <div className="row">
-                <div className="col-md-3  align-self-center ">
-                    <input type="text" className="form-control" placeholder="Search by brand..." />
-                </div>
-                <div className="col-md-9" >
-                    <ul className="nav justify-content-end align-items-center">
-                        <li className="nav-item" style={{ width: '63px' }} >
-                            <a className="nav-link" href="https://github.com/mrhrifat" target='_blank'>
-                                {<PersonIcon size='medium' fill='#000' aria-label="Add new item" />}
-                            </a>
-                        </li>
-                        <li className="nav-item" style={{ width: '70px' }} >
-                            <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank'>
-                                {<RepoForkedIcon size='medium' fill='#000' aria-label="Add new item" />}
-                            </a>
-                        </li>
-                        <li className="nav-item" style={{ width: '70px' }} >
-                            <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank'>
-                                {<StarIcon size='medium' fill='#000' aria-label="Add new item" />}
-                            </a>
-                        </li>
-                        <li className="nav-item" style={{ width: '70px' }} >
-                            <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank'>
-                                {<LawIcon size='medium' fill='#000' aria-label="Add new item" />}
-                            </a>
-                        </li>
-                        <li className="nav-item" style={{ width: '70px' }} >
-                            <a className="nav-link" href="#" target='_blank'>
-                                {<HeartIcon size='medium' fill='#000' aria-label="Add new item" />}
-                            </a>
-                        </li>
-                    </ul>
+    render() {
+        return (
+            <div className='container pb-3'>
+                <div className="row">
+                    <div className="col-md-3  align-self-center ">
+                        <input type="text" className="form-control" placeholder="Search by brand..." />
+                    </div>
+                    <div className="col-md-9" >
+                        <ul className="nav justify-content-end align-items-center">
+                            <li className="nav-item" style={{ width: '63px' }} >
+                                <a className="nav-link" href="https://github.com/mrhrifat" target='_blank'>
+                                    {<PersonIcon size='medium' fill='#000' aria-label="Add new item" />}
+                                </a>
+                            </li>
+                            <li className="nav-item" style={{ width: '70px' }} >
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank'>
+                                    {<RepoForkedIcon size='medium' fill='#000' aria-label="Add new item" />}
+                                </a>
+                            </li>
+                            <li className="nav-item" style={{ width: '70px' }} >
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank'>
+                                    {<StarIcon size='medium' fill='#000' aria-label="Add new item" />}
+                                </a>
+                            </li>
+                            <li className="nav-item" style={{ width: '70px' }} >
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank'>
+                                    {<LawIcon size='medium' fill='#000' aria-label="Add new item" />}
+                                </a>
+                            </li>
+                            <li className="nav-item" style={{ width: '70px' }} >
+                                <a className="nav-link" href="#" target='_blank'>
+                                    {<HeartIcon size='medium' fill='#000' aria-label="Add new item" />}
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Search;
