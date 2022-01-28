61c4d991ffc7f68ca7ddc0ed73c8f47e95d731bf
mrhrifat <mrhrifat383759@gmail.com>
2022-01-28 14:43:14 +0600

    Create Visitor Method & Handle with State

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,6 +1,7 @@
 import React, { Component, Suspense, lazy } from 'react'
 import simpleClass from './services/main'
 import Spinner from './components/Spinner'
+const countapi = require('countapi-js')
 const Header = lazy(() => import('./components/Header'))
 const IconCard = lazy(() => import('./components/IconCard'))
 
@@ -8,20 +9,36 @@ const IconCard = lazy(() => import('./components/IconCard'))
 
 const data = new simpleClass()
 
+
 class Root extends Component {
 
     state = {
-        data: data.getIcon()
+        data: data.getIcon(),
+        visitor: 0
+    }
+
+    componentDidMount() {
+        countapi.visits('root').then((result) => {
+                // console.log(result)
+                this.setState({
+                    visitor: result.value
+                })
+            })
+            .catch((e) => {
+                console.log(e)
+            })
     }
 
     render() {
         // console.log(this.state.data)
         const data = this.state.data
-
+        // console.log(this.state.visitor)
+        const visitor = this.state.visitor
+        // console.log(visitor)
         return (
             <>
                 <Suspense fallback={<Spinner />}>
-                    <Header />
+                    <Header visitor={visitor} />
                 </Suspense>
                 <Suspense fallback={<Spinner />}>
                     {<IconCard data={data} />}
