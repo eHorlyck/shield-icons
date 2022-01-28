a6bc0ed559bd432ce038f8de6ce0f1183c840ac7
mrhrifat <mrhrifat383759@gmail.com>
2022-01-28 14:53:59 +0600

    Add rel noopener & noreferrer to a tag

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -27,33 +27,33 @@ class Search extends Component {
                     <div className="col-md-8 " >
                         <ul className="nav justify-content-md-end justify-content-sm-center align-items-center">
                             <li className="nav-item"  >
-                                <a className="nav-link" href="https://github.com/mrhrifat" target='_blank'>
-                                    {<PersonIcon size='medium' fill='#000'  />}
+                                <a className="nav-link" href="https://github.com/mrhrifat" target='_blank' rel="noopener noreferrer">
+                                    {<PersonIcon size='medium' fill='#000' />}
                                 </a>
                             </li>
                             <li className="nav-item"  >
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank'>
-                                    {<RepoForkedIcon size='medium' fill='#000'  />}
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank' rel="noopener noreferrer">
+                                    {<RepoForkedIcon size='medium' fill='#000' />}
                                 </a>
                             </li>
                             <li className="nav-item"  >
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank'>
-                                    {<StarIcon size='medium' fill='#000'  />}
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank' rel="noopener noreferrer">
+                                    {<StarIcon size='medium' fill='#000' />}
                                 </a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank'>
-                                    {<LawIcon size='medium' fill='#000'  />}
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank' rel="noopener noreferrer">
+                                    {<LawIcon size='medium' fill='#000' />}
                                 </a>
                             </li>
                             <li className="nav-item"  >
-                                <a className="nav-link" href="#" target='_blank'>
-                                    {<BugIcon size='medium' fill='#000'  />}
+                                <a className="nav-link" href="#" target='_blank' rel="noopener noreferrer">
+                                    {<BugIcon size='medium' fill='#000' />}
                                 </a>
                             </li>
                             <li className="nav-item" >
-                                <a className="nav-link" href="#" target='_blank'>
-                                    {<HeartIcon size='medium' fill='#000'  />}
+                                <a className="nav-link" href="#" target='_blank' rel="noopener noreferrer">
+                                    {<HeartIcon size='medium' fill='#000' />}
                                 </a>
                             </li>
                         </ul>
