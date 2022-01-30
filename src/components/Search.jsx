d6399739999b5fa4a294c850427492b07a3ecc5a
mrhrifat <mrhrifat383759@gmail.com>
2022-01-30 22:29:50 +0600

    Remove duplication props with template string

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -34,32 +34,32 @@ class Search extends Component {
 
                     <div className="col-md-7" >
                         <ul className="nav justify-content-md-end justify-content-sm-center">
-                            <li className="nav-item" data-toggle="tooltip" title='Author' className={classes.hoverIcon} >
+                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Author'>
                                 <a className="nav-link" href="https://github.com/mrhrifat" target='_blank' rel="noopener noreferrer">
                                     {<PersonIcon size='22' fill='#000' />}
                                 </a>
                             </li>
-                            <li className="nav-item" data-toggle="tooltip" title='Forked Repository' className={classes.hoverIcon} >
+                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Forked Repository'>
                                 <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank' rel="noopener noreferrer">
                                     {<RepoForkedIcon size='22' fill='#000' />}
                                 </a>
                             </li>
-                            <li className="nav-item" data-toggle="tooltip" title='Star Repository' className={classes.hoverIcon} >
+                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Star Repository'>
                                 <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank' rel="noopener noreferrer">
                                     {<StarIcon size='22' fill='#000' />}
                                 </a>
                             </li>
-                            <li className="nav-item" data-toggle="tooltip" title='License' className={classes.hoverIcon} >
+                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='License'>
                                 <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank' rel="noopener noreferrer">
                                     {<LawIcon size='22' fill='#000' />}
                                 </a>
                             </li>
-                            <li className="nav-item" data-toggle="tooltip" title='Bug Report' className={classes.hoverIcon} >
+                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Bug Report'>
                                 <a className="nav-link" href="https://github.com/mrhrifat/web-badge/issues/new" target='_blank' rel="noopener noreferrer">
                                     {<BugIcon size='22' fill='#000' />}
                                 </a>
                             </li>
-                            <li className="nav-item" data-toggle="tooltip" title='Support' className={classes.hoverIcon} >
+                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Support'>
                                 <a className="nav-link" href="https://github.com/mrhrifat/web-badge#support" target='_blank' rel="noopener noreferrer">
                                     {<HeartIcon size='22' fill='#000' />}
                                 </a>
