7b6b82d3aa6412c3a00443ddbbbfdd6bd128cfb9
mrhrifat <mrhrifat383759@gmail.com>
2022-01-30 22:41:22 +0600

    Make seperator filter & map

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -24,53 +24,55 @@ const IconCard = ({ data }) => {
 
 
                 <div className="row">
-                    {data.filter(item => {
-                        if (search === '') {
-                            return item
-                        } else if (item.title.toLowerCase().includes(search.toLowerCase())) {
-                            return item
-                        }
-                    }).map((item) => (
-                        <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
-                            <div className='card' style={{ borderRadius: '1rem' }}>
-                                <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
-                                    <Suspense fallback={<Spinner />}>
-                                        <SVGIcon
-                                            svg={item.svg}
-                                            alt={item.title}
-                                        />
-                                    </Suspense>
-                                </div>
-                                < >
-                                    <Suspense fallback={<Spinner />}>
-                                        <Badge title={item.title} hex={item.hex} />
-                                    </Suspense>
-                                </>
+                    {data
+                        .filter((item) => {
+                            if (search === '') {
+                                return item
+                            } else if (item.title.toLowerCase().includes(search.toLowerCase())) {
+                                return item
+                            }
+                        })
+                        .map((item) => (
+                            <div className="col-md-3 col-sm-4 col-xs-6 my-2" key={item.slug}>
+                                <div className='card' style={{ borderRadius: '1rem' }}>
+                                    <div className='card-img-fluid' style={{ width: '20%', margin: '10px auto', display: 'block' }}>
+                                        <Suspense fallback={<Spinner />}>
+                                            <SVGIcon
+                                                svg={item.svg}
+                                                alt={item.title}
+                                            />
+                                        </Suspense>
+                                    </div>
+                                    < >
+                                        <Suspense fallback={<Spinner />}>
+                                            <Badge title={item.title} hex={item.hex} />
+                                        </Suspense>
+                                    </>
 
-                                <div className="d-flex">
-                                    <div className="card-footer w-75"
-                                        style={{ backgroundColor: `#${item.hex}`, cursor: 'pointer' }}>
-                                        <small className={`${classes.custom} text-light text-center`}>
-                                            {/* {
+                                    <div className="d-flex">
+                                        <div className="card-footer w-75"
+                                            style={{ backgroundColor: `#${item.hex}`, cursor: 'pointer' }}>
+                                            <small className={`${classes.custom} text-light text-center`}>
+                                                {/* {
                                                 `#${item.hex}`
 
                                             } */}
-                                            < CopyHex hex={item.hex} />
-                                        </small>
+                                                < CopyHex hex={item.hex} />
+                                            </small>
+                                        </div>
+                                        <>
+                                            < DownloadLink
+                                                label={<DownloadIcon fill='#333' />}
+                                                filename={`${item.title}.svg`}
+                                                exportFile={() => item.svg}
+                                                className={`${classes.hoverIcon} card-footer w-25`}
+                                                style={{ cursor: 'pointer', borderRadius: '0 0 14px 0' }}
+                                            />
+                                        </>
                                     </div>
-                                    <>
-                                        < DownloadLink
-                                            label={<DownloadIcon fill='#333' />}
-                                            filename={`${item.title}.svg`}
-                                            exportFile={() => item.svg}
-                                            className={`${classes.hoverIcon} card-footer w-25`}
-                                            style={{ cursor: 'pointer', borderRadius: '0 0 14px 0' }}
-                                        />
-                                    </>
                                 </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
                 </div>
             </div>
         </>
