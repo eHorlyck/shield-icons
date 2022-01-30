1b272f567eaa4127fdedd085f8b5d39387b7886c
mrhrifat <mrhrifat383759@gmail.com>
2022-01-30 21:43:03 +0600

    Create String Heading & Remove Duplicate Class Name with Template String

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({ visitor }) => {
         <div className='container py-5'>
             <div className="row">
                 <div className="col-md-6" style={{ fontWeight: '500' }}>
-                    <h3>Web Badge</h3>
+                    <h3><strong>Web Badge</strong></h3>
                     <p className='m-0'>2152 Free Badges & <abbr title='Scalable Vector Graphics' className='initialism'>SVG</abbr> of popular brands</p>
                     {/* <div className="">
                         <EyeIcon /> {millify(visitor)} Visitor
@@ -18,22 +18,22 @@ const Header = ({ visitor }) => {
                 </div>
                 <div className="col-md-6">
                     <ul className={`nav justify-content-md-end justify-content-sm-center ${classes.list}`}>
-                        <li className="nav-item" style={{ width: '65px' }} className={classes.hoverIcon}  >
+                        <li className={`nav-item ${classes.hoverIcon}`} style={{ width: '65px' }}>
                             <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target="_blank" rel="noopener noreferrer">
                                 {<InlineSVG src={simpleIcons.Get('github').svg} />}
                             </a>
                         </li>
-                        <li className="nav-item" style={{ width: '65px' }} className={classes.hoverIcon} >
+                        <li className={`nav-item ${classes.hoverIcon}`} style={{ width: '65px' }}>
                             <a className="nav-link" href="https://ko-fi.com/mrhrifat" target="_blank" rel="noopener noreferrer">
                                 {<InlineSVG src={simpleIcons.Get('kofi').svg} />}
                             </a>
                         </li>
-                        <li className="nav-item" style={{ width: '65px' }} className={classes.hoverIcon} >
+                        <li className={`nav-item ${classes.hoverIcon}`} style={{ width: '65px' }}>
                             <a className="nav-link" href="https://patreon.com/mrhrifat" target="_blank" rel="noopener noreferrer">
                                 {<InlineSVG src={simpleIcons.Get('patreon').svg} />}
                             </a>
                         </li>
-                        <li className="nav-item" style={{ width: '65px' }} className={classes.hoverIcon} >
+                        <li className={`nav-item ${classes.hoverIcon}`} style={{ width: '65px' }}>
                             <a className="nav-link" href="https://buymeacoffee.com/mrhrifat" target="_blank" rel="noopener noreferrer">
                                 {<InlineSVG src={simpleIcons.Get('buymeacoffee').svg} />}
                             </a>
