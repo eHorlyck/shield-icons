237d84dd3025bcb6814afba4f2d4912eb3aeb5b8
mrhrifat <mrhrifat383759@gmail.com>
2022-02-02 17:15:19 +0600

    Remove unneded comments code

--- src/services/main.js
+++ src/services/main.js
@@ -1,14 +1,12 @@
 const simpleIcons = require('simple-icons');
 const slug = require('../data/slug.json')
-// console.log(simpleIcons.Get('hackerearth'))
+
 
 export default class simpleClass {
 
     getIcon() {
-        // console.log(slug.brand)
+        
         return slug.brand.map(item => {
-            // console.log(item)
-            // console.log(`${item.slug}`)
             const obj = simpleIcons.Get(`${item.slug}`)
             return {
                 slug: obj.slug,
