c22c49bf77c513728ef860192077e27df9162b17
mrhrifat <mrhrifat383759@gmail.com>
2022-02-02 17:21:34 +0600

    Remove commented code or statemenet

--- src/components/SVGIcon.jsx
+++ src/components/SVGIcon.jsx
@@ -3,10 +3,7 @@ import InlineSVG from 'svg-inline-react';
 
 
 const SVGIcon = ({ svg }) => {
-    // console.log(data[0].svg)
-    // console.log(svg)
     return (
-        // console.log(item.svg)
         <InlineSVG src={svg} />
     )
 }
