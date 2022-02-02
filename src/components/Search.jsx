dbed7a52a8f161a4bcc16ba0062c62f14355179b
mrhrifat <mrhrifat383759@gmail.com>
2022-02-02 23:19:17 +0600

    Change Classess Module Path & Name & Fix Some Space

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { RepoForkedIcon, PersonIcon, LawIcon, StarIcon, HeartIcon, BugIcon } from '@primer/octicons-react'
-import classes from '../styles/search.module.css'
+import classes from '../styles/all.module.css'
 
 
 class Search extends Component {
@@ -18,7 +18,7 @@ class Search extends Component {
         const { type } = this.props
 
         return (
-            <div className={`${classes.navbar} container pb-4 pt-1 `}>
+            <div className={`container pb-4 pt-1 ${classes.navbar}`}>
                 <div className="row">
                     <div className="col-md-4">
                         <input
@@ -29,53 +29,49 @@ class Search extends Component {
                         />
                     </div>
 
-                    <div className="col-md-3">
-                        <div className="row" >
-                            <div className="col-md-5">
-                                <div className="btn-group" role="group" style={{ border: '1px solid #6c757d', borderRadius: '0.25rem' }}>
-                                    <button className={type === 'svg' ? `btn btn-secondary ${classes.custom}` : `btn ${classes.custom}`} value='svg'
-                                        onClick={this.handleDownload}>
-                                        SVG
-                                    </button>
-                                    <button className={type === 'png' ? `btn btn-secondary ${classes.custom}` : `btn ${classes.custom}`} value='png'
-                                        onClick={this.handleDownload}>
-                                        PNG
-                                    </button>
-                                </div>
-                            </div>
+                    <div className={`col-md-3 ${classes.download}`}>
+                        <div className="btn-group" role="group" style={{ border: '1px solid #6c757d', borderRadius: '0.25rem' }}>
+                            <button className={type === 'svg' ? `btn btn-secondary ${classes.option}` : `btn ${classes.option}`} value='svg'
+                                onClick={this.handleDownload}>
+                                SVG
+                            </button>
+                            <button className={type === 'png' ? `btn btn-secondary ${classes.option}` : `btn ${classes.option}`} value='png'
+                                onClick={this.handleDownload}>
+                                PNG
+                            </button>
                         </div>
                     </div>
 
                     <div className="col-md-5" >
-                        <ul className="nav justify-content-md-end justify-content-sm-center">
-                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Author'>
-                                <a className="nav-link" href="https://github.com/mrhrifat" target='_blank' rel="noopener noreferrer" style={{ padding: '7px 10px' }}>
-                                    {<PersonIcon size='22' fill='#000' />}
+                        <ul className={`nav justify-content-md-end justify-content-sm-center ${classes.searchNav}`}>
+                            <li className="nav-item" data-toggle="tooltip" title='Author'>
+                                <a className="nav-link" href="https://github.com/mrhrifat" target='_blank' rel="noopener noreferrer">
+                                    {<PersonIcon size='22' />}
                                 </a>
                             </li>
-                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Forked Repository'>
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank' rel="noopener noreferrer" style={{ padding: '7px 10px' }}>
-                                    {<RepoForkedIcon size='22' fill='#000' />}
+                            <li className="nav-item" data-toggle="tooltip" title='Forked Repository'>
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/fork" target='_blank' rel="noopener noreferrer">
+                                    {<RepoForkedIcon size='22' />}
                                 </a>
                             </li>
-                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Star Repository'>
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank' rel="noopener noreferrer" style={{ padding: '7px 10px' }}>
-                                    {<StarIcon size='22' fill='#000' />}
+                            <li className="nav-item" data-toggle="tooltip" title='Star Repository'>
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge" target='_blank' rel="noopener noreferrer">
+                                    {<StarIcon size='22' />}
                                 </a>
                             </li>
-                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='License'>
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank' rel="noopener noreferrer" style={{ padding: '7px 10px' }}>
-                                    {<LawIcon size='22' fill='#000' />}
+                            <li className="nav-item" data-toggle="tooltip" title='License'>
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/blob/master/LICENSE.md" target='_blank' rel="noopener noreferrer">
+                                    {<LawIcon size='22' />}
                                 </a>
                             </li>
-                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Bug Report'>
-                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/issues/new" target='_blank' rel="noopener noreferrer" style={{ padding: '7px 10px' }}>
-                                    {<BugIcon size='22' fill='#000' />}
+                            <li className="nav-item" data-toggle="tooltip" title='Bug Report'>
+                                <a className="nav-link" href="https://github.com/mrhrifat/web-badge/issues/new" target='_blank' rel="noopener noreferrer">
+                                    {<BugIcon size='22' />}
                                 </a>
                             </li>
-                            <li className={`nav-item ${classes.hoverIcon}`} data-toggle="tooltip" title='Support'>
+                            <li className="nav-item" data-toggle="tooltip" title='Support'>
                                 <a className="nav-link" href="https://github.com/mrhrifat/web-badge#support" target='_blank' rel="noopener noreferrer">
-                                    {<HeartIcon size='22' fill='#000' />}
+                                    {<HeartIcon size='22' />}
                                 </a>
                             </li>
                         </ul>
