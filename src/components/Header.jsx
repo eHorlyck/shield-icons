76750b2b36186f230933f7668eb6f5a96c70ef4c
mrhrifat <mrhrifat383759@gmail.com>
2022-02-02 17:17:42 +0600

    Remove commented code or statemenet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import InlineSVG from 'svg-inline-react';
 import classes from '../styles/header.module.css';
-// import { EyeIcon } from '@primer/octicons-react';
-// import millify from 'millify';
 const simpleIcons = require('simple-icons');
 
-const Header = ({ visitor }) => {
+const Header = ({ }) => {
     return (
         <div className='container py-5'>
             <div className="row">
                 <div className="col-md-6" style={{ fontWeight: '500' }}>
                     <h3><strong>Web Badge</strong></h3>
                     <p className='m-0'>2152 Free Badges & <abbr title='Scalable Vector Graphics' className='initialism'>SVG</abbr> of popular brands</p>
-                    {/* <div className="">
-                        <EyeIcon /> {millify(visitor)} Visitor
-                    </div> */}
                 </div>
                 <div className="col-md-6">
                     <ul className={`nav justify-content-md-end justify-content-sm-center ${classes.list}`}>
