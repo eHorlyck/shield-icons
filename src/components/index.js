daadb911817b174ff4593147d51c8cba6a11bf69
mrhrifat <mrhrifat383759@gmail.com>
2022-04-01 04:16:12 +0600

    refactor: index

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,3 @@
-export {
-    default as Badge
-}
-from './Badge';
-export {
-    default as CopyHex
-}
-from './CopyHex';
-export {
-    default as Spinner
-}
-from './Spinner';
+export { default as Badge } from "./Badge";
+export { default as CopyHex } from "./CopyHex";
+export { default as Spinner } from "./Spinner";
