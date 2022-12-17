e111ad28f59ebfd7c4319ccbca523efee0c9a06e
mrhrifat <mrhrifat383759@gmail.com>
2022-12-17 23:44:43 +0600

    remove: all existing code

--- src/services/main.js
+++ src/services/main.js
@@ -1,16 +1,1 @@
-const simpleIcons = require("simple-icons");
-const slug = require("../data/test.json");
-
-export default class simpleClass {
-  getIcon() {
-    return slug.brand.map((item) => {
-      const obj = simpleIcons.Get(`${item.slug}`);
-      return {
-        slug: obj.slug,
-        title: obj.title,
-        hex: obj.hex,
-        svg: obj.svg,
-      };
-    });
-  }
-}
+import * as icons from "simple-icons";
